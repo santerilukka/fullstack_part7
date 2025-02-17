8f4646830030bb3eb36362ec2e8d650110001e54
santerilukka <156800715+santerilukka@users.noreply.github.com>
2025-02-17 20:22:39 +0200

    7.9: koodin automaattinen muotoilu

--- ultimate-hooks/src/App.jsx
+++ ultimate-hooks/src/App.jsx
@@ -11,7 +11,7 @@ const useField = (type) => {
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
@@ -19,26 +19,22 @@ const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
   useEffect(() => {
-    axios.get(baseUrl)
-      .then(response => {
-        setResources(response.data)
-      })
+    axios.get(baseUrl).then((response) => {
+      setResources(response.data)
+    })
   }, [])
 
   const create = (resource) => {
-    axios.post(baseUrl, resource)
-      .then(response => {
-        setResources(resources.concat(response.data))
-      })
+    axios.post(baseUrl, resource).then((response) => {
+      setResources(resources.concat(response.data))
+    })
   }
 
   const service = {
-    create
+    create,
   }
 
-  return [
-    resources, service
-  ]
+  return [resources, service]
 }
 
 const App = () => {
@@ -53,10 +49,10 @@ const App = () => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
   }
 
   return (
@@ -66,15 +62,21 @@ const App = () => {
         <input {...content} />
         <button>create</button>
       </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
+        name <input {...name} /> <br />
         number <input {...number} />
         <button>create</button>
       </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
     </div>
   )
 }
